761e30fde5551e349695bc1f471ec85a8c7d8be3
Shivanibisen07 <bisenshivani3@gmail.com>
2024-08-15 23:28:00 +0530

    Files till 10th Aug are added

---
+++ src/exe_10th Aug_2024/Lab004.py
@@ -0,0 +1,4 @@
+#Write a programm to find maximum number between two integers
+print(max(34,89,55))
+print(max(34,89,55, 0.01, -76))#Write a programm to find maximum number between two integers
+print(max(34,89,55))
---
+++ src/exe_10th Aug_2024/Lab006.py
@@ -0,0 +1,11 @@
+#import keyword
+#print(keyword.kwlist)
+
+age=65
+print(age)
+
+abc123=65
+##123qpr=89
+_shivani="07"
+_123="shivani"
+print(_shivani,_123)
