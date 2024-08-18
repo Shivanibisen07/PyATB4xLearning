eb692395d211fdf80fb47cb1088c3975e7c578a6
Shivanibisen07 <bisenshivani3@gmail.com>
2024-08-18 11:45:00 +0530

    Files till 13th Aug are added

---
+++ src/exe_13th Aug_2024/List and String.py
@@ -0,0 +1,19 @@
+my_shopping_list = ["milk", "bread"] #list
+
+#str
+name="Shivani"
+print(type(name))
+print(name.lower())
+print(name.upper())
+print(len(name))
+
+how_many_apply_i_have=None            #NoneType - Datatype
+print(type(how_many_apply_i_have))
+
+#id ------- where it is stored in memory - address
+age = 10
+age2=10
+age = 11
+print(id(age))
+print(id(age2))
+print(id(age))
---
+++ src/exe_13th Aug_2024/Task2- max, calculation, formatting.py
@@ -0,0 +1,14 @@
+a = int(input("Enter Number 1 "))
+b = int(input("Enter Number 2 "))
+
+Max=max(a,b)
+print(f"Maximum number is {Max}")
+
+Power =pow(a,b)
+print(f"Power of number is {Power}")
+
+c=print("Multiplication is ", a*b)
+d=print("Additon is", a+b)
+e=print("Substractio is", a-b)
+f=print("Division is", a/b)
+
