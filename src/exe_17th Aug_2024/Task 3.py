a8203513da54cd0ac6cf5e90ee3bbd85f54cfb95
Shivanibisen07 <bisenshivani3@gmail.com>
2024-09-07 19:07:11 +0530

    Files till 17th Aug are added

---
+++ src/exe_17th Aug_2024/Task 3.py
@@ -0,0 +1,10 @@
+### Task #3
+#- Explain the difference between the = operator and the == operator in Python.
+#Answer - = is assignment operator and == is comparision operator
+
+#- What does the ** operator do in Python, and how is it used?
+#** is use to give power to any number
+#Example - 2**3 = 2^3 = 8
+
+#- What does the ^ operator do in Python, and in what context is it commonly used?
+# ^ is used to perform XOR
---
+++ src/exe_17th Aug_2024/greater less_Task 5.py
@@ -0,0 +1,11 @@
+### Task #5 -Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+a = int(input("Enter the first number-"))
+b = int(input("Enter the second number-"))
+if a>b:
+    print("First number is greater than second number")
+elif a<b:
+    print("First number is less than second number")
+else:
+    print("First number is equals to second number")
+
